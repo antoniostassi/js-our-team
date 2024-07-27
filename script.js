2249d577ed7c80f83e9282cba438511ca42f86a4
antoniostassi <stassiant@outlook.it>
2024-07-27 16:05:40 +0200

    JS | Creation of Objects

---
+++ script.js
@@ -0,0 +1,49 @@
+// DATI PER ESERCIZIO:
+
+// Name            Role                    Image
+// Wayne Barnett	Founder & CEO	        wayne-barnett-founder-ceo.jpg
+// Angela Caroll	Chief Editor	        angela-caroll-chief-editor.jpg
+// Walter Gordon	Office Manager	        walter-gordon-office-manager.jpg
+// Angela Lopez	Social Media Manager	angela-lopez-social-media-manager.jpg
+// Scott Estrada	Developer	            scott-estrada-developer.jpg
+// Barbara Ramos	Graphic Designer	    barbara-ramos-graphic-designer.jpg
+
+
+let staffUsers = [
+    {
+        name: "Wayne",
+        surname: "Barnett",
+        role: "Founder & Ceo",
+        image: "wayne-barnett-founder-ceo.jpg"
+    },
+    {
+        name: "Angela",
+        surname: "Caroll",
+        role: "Chief Editor",
+        image: "angela-caroll-chief-editor.jpg"
+    },
+    {
+        name: "Walter",
+        surname: "Gordon",
+        role: "Office Manager",
+        image: "walter-gordon-office-manager.jpg"
+    },
+    {
+        name: "Angela",
+        surname: "Lopez",
+        role: "Social Media Manager",
+        image: "angela-lopez-social-media-manager.jpg"
+    },
+    {
+        name: "Scott",
+        surname: "Estrada",
+        role: "Developer",
+        image: "scott-estrada-developer.jpg"
+    },
+    {
+        name: "Barbara",
+        surname: "Ramos",
+        role: "Graphic Designer",
+        image: "barbara-ramos-graphic-designer.jpg"
+    }
+]
